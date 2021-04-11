21385f2aeb9d387bd6f15f702a33af3d07b13ce0
Sinan Ulusan <sinanulusan99@gmail.com>
2021-04-11 20:39:20 +0300

    first commit

---
+++ card.js
@@ -0,0 +1,8 @@
+function result() {
+    document.getElementById("price-text").style.visibility= "hidden";
+    document.getElementById("price-visibility").style.visibility= "visible";
+}
+function notresult() {
+    document.getElementById("price-text").style.visibility= "visible";
+    document.getElementById("price-visibility").style.visibility= "hidden";
+}
